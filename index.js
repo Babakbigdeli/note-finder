97ae80eddb619497a1eddcf23e8521a15994ea97
Babak <babakbigdeli@gmail.com>
2024-06-26 22:56:17 +0100

    The main note finder function created and tested

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const formula = [W, W, H, W, W, W, H];
 const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
 
+function noteFinder(key) {
+    const scaleNotes = [];
+    // we find the index of the key and set it as the start point of the loop
+    let startPoint = notes.indexOf(key);
+    // the first note is the root note. so we pushing that in 
+    scaleNotes.push(key);
+    formula.forEach(step => {
+        // after each iteration we set a new start point. also by using modulus we handle the wrapping around of the note array
+        startPoint = (startPoint + step) % notes.length;
+        scaleNotes.push(notes[startPoint]);
+    });
+    console.log(scaleNotes);
+    return scaleNotes;
+}
