6500058f85173ce515a5f4c904349efab3da540f
Babak <babakbigdeli@gmail.com>
2024-09-22 20:10:38 +0100

    param added to the main function to cover for all different possibilities

--- script.js
+++ script.js
@@ -15,13 +15,14 @@ const formula = {major : [W, W, H, W, W, W, H],
 const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
 
-function noteFinder(key) {
+function noteFinder(key, scaleType) {
     const scaleNotes = [];
+    const appliedFormula = formula[scaleType]
     // we find the index of the key and set it as the start point of the loop
     let startPoint = notes.indexOf(key);
     // the first note is the root note. so we pushing that in 
     scaleNotes.push(key);
-    formula.forEach(step => {
+    appliedFormula.forEach(step => {
         // after each iteration we set a new start point. also by using modulus we handle the wrapping around of the note array
         startPoint = (startPoint + step) % notes.length;
         scaleNotes.push(notes[startPoint]);
